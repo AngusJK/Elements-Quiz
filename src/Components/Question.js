e7cd4a378d481d17e4d61a32e3b0b05cb457f4dc
Angus Kellett <anguskellett@hotmail.com>
2021-06-01 22:58:54 +0000

    creates p with question and h1 with element symbol

---
+++ src/Components/Question.js
@@ -0,0 +1,10 @@
+const question = function() {
+  return (
+    <div className="question">
+      <p>Name the element with this symbol:</p>
+      <h1>Hg</h1>
+    </div>
+  )
+}
+
+export default question;
