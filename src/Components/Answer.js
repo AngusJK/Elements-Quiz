136bdc1a53e31b378b62d2d17238979286fec2d3
Angus Kellett <anguskellett@hotmail.com>
2021-06-01 22:58:10 +0000

    creates form input and submit button

---
+++ src/Components/Answer.js
@@ -0,0 +1,10 @@
+const answer = function() {
+  return(
+    <form action="" className="answer">
+      <input type="text" placeholder="type your answer here" />
+      <button>Submit</button>
+    </form>
+  )
+}
+
+export default answer;
