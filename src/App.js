cc4b3a785bba71f9bf79924d787bd36674e76209
Angus Kellett <anguskellett@hotmail.com>
2021-06-03 03:28:23 +0000

    creates sample list of 5 elements and passes one of them to Question

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+import React, { useState } from 'react';
 import './App.css';
 import Header from './Components/Header';
 import Question from './Components/Question';
 import Answer from './Components/Answer';
 
 function App() {
+  const elements = [
+  { atno: 1, name: 'hydrogen', symbol: 'H' }, 
+  { atno: 2, name: 'helium', symbol: 'He' }, 
+  { anto: 3, name: 'lithium', symbol: 'Li'},
+  { atno: 4, name: 'beryllium', symbol: 'Be' }, 
+  { atno: 5, name: 'boron', symbol: 'B'}
+  ]
   return (
     <div className="container">
       <Header title="The Elements"/>
-      <Question />
+      <Question hydrogen={elements[0].symbol}/>
       <Answer />   
       </div>
   );
