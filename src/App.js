338f5b418f060e0e5e25d442aeca5197ec0e6d97
Angus Kellett <anguskellett@hotmail.com>
2021-06-03 03:47:27 +0000

    creates currentQuestion variable and setCurrentQuestion method

--- src/App.js
+++ src/App.js
@@ -6,16 +6,21 @@ import Answer from './Components/Answer';
 
 function App() {
   const elements = [
-  { atno: 1, name: 'hydrogen', symbol: 'H' }, 
-  { atno: 2, name: 'helium', symbol: 'He' }, 
-  { anto: 3, name: 'lithium', symbol: 'Li'},
-  { atno: 4, name: 'beryllium', symbol: 'Be' }, 
-  { atno: 5, name: 'boron', symbol: 'B'}
+    { atno: 1, name: 'hydrogen', symbol: 'H' }, 
+    { atno: 2, name: 'helium', symbol: 'He' }, 
+    { anto: 3, name: 'lithium', symbol: 'Li'},
+    { atno: 4, name: 'beryllium', symbol: 'Be' }, 
+    { atno: 5, name: 'boron', symbol: 'B'}
   ]
+  const [currentQuestion, setCurrentQuestion] = useState('gee whiz');
   return (
     <div className="container">
       <Header title="The Elements"/>
-      <Question hydrogen={elements[0].symbol}/>
+      <Question 
+        currentQuestion={currentQuestion}
+        setCurrentQuestion={setCurrentQuestion}
+        elements={elements}  
+      />
       <Answer />   
       </div>
   );
