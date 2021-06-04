3bc02981fb847326001e81a9c3c3a88b24ccb40c
Angus Kellett <anguskellett@hotmail.com>
2021-06-04 17:09:18 +0000

    creates stage to hold symbol

--- src/Components/Question.js
+++ src/Components/Question.js
@@ -2,14 +2,16 @@
 
 const question = function({elements, currentElement, setCurrentElement}) {
   const setNewSymbol = (e) => {
-    let num = Math.floor(Math.random() * 5);
+    let num = Math.floor(Math.random() * 10);
     setCurrentElement(elements[num]);
   }
   return (
     <div className="question">
       <button onClick={setNewSymbol}>New symbol</button>
       <p>Name the element with this symbol:</p>
-      <h1>{currentElement.symbol}</h1>
+      <div className="stage">
+        <h1>{currentElement.symbol}</h1>
+      </div>
     </div>
   )
 }
