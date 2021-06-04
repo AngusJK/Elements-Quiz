42fcaee9ef0db9f79d2d2c6d6875c467b9f9de15
Angus Kellett <anguskellett@hotmail.com>
2021-06-04 19:31:38 +0000

    now contains all form HTML and methods

--- src/Components/Question.js
+++ src/Components/Question.js
@@ -1,17 +1,27 @@
-//import { useState } from "react";
-
-const question = function({elements, currentElement, setCurrentElement}) {
+const question = function({ inputText, setInputText, elements, currentElement, setCurrentElement }) {
   const setNewSymbol = (e) => {
-    let num = Math.floor(Math.random() * 10);
+    let num = Math.floor(Math.random() * 30);
     setCurrentElement(elements[num]);
+  }
+  const submitAnswerHandler = (e) => {
+    e.preventDefault();
+    console.log(inputText);
+    setInputText('');
+    setNewSymbol();
+  }
+  const inputTextHandler = (e) => {
+    setInputText(e.target.value.trim().toLowerCase());
   }
   return (
     <div className="question">
-      <button onClick={setNewSymbol}>New symbol</button>
+      <button onClick={setNewSymbol}>Start Game</button>
       <p>Name the element with this symbol:</p>
       <div className="stage">
         <h1>{currentElement.symbol}</h1>
       </div>
+      <form action="" className="answer" onSubmit={submitAnswerHandler}>
+        <input onChange={inputTextHandler} type="text" placeholder="type your answer here" />
+      </form>
     </div>
   )
 }
