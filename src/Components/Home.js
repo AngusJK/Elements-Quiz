4489e719ae3072c8a1e4532a62cdcbd58382c891
Angus Kellett <anguskellett@hotmail.com>
2021-06-14 23:15:19 +0000

    imports link and adds link to each li

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import '../App.css';
 
-function Home() {
+const Home = function() {
   return(
-    <div>
+    <div className="home">
       <h1>Home Page</h1>
       <h3>Enter your Name</h3>
       <h3>Choose a game</h3>
-      <ul>
-        <li>Game 1</li>
-        <li>Game 2</li>
-        <li>Game 3</li>
+      <ul className="nav-links">
+        <Link to="/guessname">
+          <li>Game 1</li>
+        </Link>
+        <Link to="/guessnumber">
+          <li>Game 2</li>
+        </Link>
+        <Link to="/guesssymbol">
+          <li>Game 3</li>
+        </Link>
       </ul>
     </div>
   )
