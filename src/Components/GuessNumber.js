97b1e429fe5d48f092147be35ff1ef3b1fcdd4a4
Angus Kellett <anguskellett@hotmail.com>
2021-06-15 00:06:01 +0000

    adds link to home page

--- src/Components/GuessNumber.js
+++ src/Components/GuessNumber.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const GuessNumber = function() {
   return(
     <div>
-      <h1>Guess the Number</h1>
+      <h1 className="home">Guess the Number</h1>
+      <Link to="/">
+        <button>Home</button>
+      </Link>
     </div>
   )
 }
