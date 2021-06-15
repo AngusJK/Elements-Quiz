7d0e7452c647c1e596c5928162f373931afffd4b
Angus Kellett <anguskellett@hotmail.com>
2021-06-15 00:06:17 +0000

    adds link to home page

--- src/Components/GuessSymbol.js
+++ src/Components/GuessSymbol.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const GuessSymbol = function() {
   return(
     <div>
-      <h1>Guess the Symbol</h1>
+      <h1 className="home">Guess the Symbol</h1>
+      <Link to="/">
+        <button>Home</button>
+      </Link>
     </div>
   )
 }
