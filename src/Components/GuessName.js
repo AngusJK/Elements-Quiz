20dc5b9abd99a4cfa98dc6c7cd9ddeab4402fa60
Angus Kellett <anguskellett@hotmail.com>
2021-06-15 21:38:48 +0000

    resets board when user exits

--- src/Components/GuessName.js
+++ src/Components/GuessName.js
@@ -11,7 +11,9 @@ const GuessName = function({
     setQuestionsAsked,
     correctAnswers,
     setCorrectAnswers,
-    userName
+    userName,
+    gameOver,
+    setGameOver
     }) {
   const setNewSymbol = () => {
     let num = Math.floor(Math.random() * 50);
@@ -20,8 +22,10 @@ const GuessName = function({
   const submitAnswerHandler = (e) => {
     e.preventDefault();
     setQuestionsAsked(questionsAsked + 1);
-    if(currentElement.name === inputText) {
+    if (currentElement.name === inputText) {
       setCorrectAnswers(correctAnswers + 1);
+    } else {
+      setGameOver(true);
     }
     e.target.reset();
     setNewSymbol();
@@ -33,26 +37,39 @@ const GuessName = function({
     setQuestionsAsked(0);
     setCorrectAnswers(0);
     setCurrentElement('');
+    setGameOver(false);
+  }
+  if (gameOver) {
+    return (
+      <div>
+        <h1>GAME OVER</h1>
+        <h3>Your score: {correctAnswers}</h3>
+        <Link to="/">
+          <button onClick={reset}>Home</button>
+        </Link>
+      </div>
+    )
+  } else {
+    return (
+      <div className="question">
+        <h3>Welcome, {userName}. Good luck!</h3>
+        <button className="start-game" onClick={setNewSymbol}>Start Game</button>
+        <p>Name the element with this symbol:</p>
+        <div className="stage">
+          <h1>{currentElement.symbol}</h1>
+        </div>
+        <form action="" className="answer" onSubmit={submitAnswerHandler}>
+          <input onChange={inputTextHandler} type="text" placeholder="type your answer here" />
+        </form>
+        <h3>Questions answered: {questionsAsked}</h3>
+        <button className="reset" onClick={reset}>Reset</button>
+        <Link to="/">
+          <button>Home</button>
+        </Link>
+      </div>
+    )
   }
   
-  return (
-    <div className="question">
-      <h3>Welcome, {userName}. Good luck!</h3>
-      <button className="start-game" onClick={setNewSymbol}>Start Game</button>
-      <p>Name the element with this symbol:</p>
-      <div className="stage">
-        <h1>{currentElement.symbol}</h1>
-      </div>
-      <form action="" className="answer" onSubmit={submitAnswerHandler}>
-        <input onChange={inputTextHandler} type="text" placeholder="type your answer here" />
-      </form>
-      <h3>Your score: {correctAnswers}/{questionsAsked}</h3>
-      <button className="reset" onClick={reset}>Reset</button>
-      <Link to="/">
-        <button>Home</button>
-      </Link>
-    </div>
-  )
 }
 
 export default GuessName;
