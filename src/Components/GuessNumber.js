f54377f5eed16d3d4c34710e4f08e2ed79f037d2
Angus Kellett <anguskellett@hotmail.com>
2021-06-15 05:58:54 +0000

    adds capitalize method

--- src/Components/GuessNumber.js
+++ src/Components/GuessNumber.js
@@ -1,15 +1,60 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const GuessNumber = function() {
-  return(
+const GuessNumber = function({ 
+  inputText, 
+  setInputText, 
+  elements, 
+  currentElement, 
+  setCurrentElement,
+  questionsAsked, 
+  setQuestionsAsked,
+  correctAnswers,
+  setCorrectAnswers
+  }) {
+const setNewElementName = () => {
+  let num = Math.floor(Math.random() * 50);
+  setCurrentElement(elements[num]);
+}
+const submitAnswerHandler = (e) => {
+  e.preventDefault();
+  setQuestionsAsked(questionsAsked + 1);
+  if(currentElement.atno.toString() === inputText) {
+    setCorrectAnswers(correctAnswers + 1);
+  }
+  e.target.reset();
+  setNewElementName();
+}
+const inputTextHandler = (e) => {
+  setInputText(e.target.value.trim());
+}
+const reset = () => {
+  setQuestionsAsked(0);
+  setCorrectAnswers(0);
+  setCurrentElement('');
+}
+const capitalize = function(s) {
+  if (typeof(s) !== 'string') return "";
+  return s.charAt(0).toUpperCase() + s.slice(1);
+}
+
+return (
+  <div className="question">
+    <button className="start-game" onClick={setNewElementName}>Start Game</button>
+    <p>What is the Atomic Number of this element?:</p>
     <div>
-      <h1 className="home">Guess the Number</h1>
-      <Link to="/">
-        <button>Home</button>
-      </Link>
+      <h1>{capitalize(currentElement.name)}</h1>
     </div>
-  )
+    <form action="" className="answer" onSubmit={submitAnswerHandler}>
+      <input onChange={inputTextHandler} type="text" placeholder="type your answer here" />
+    </form>
+    <h3>Your score: {correctAnswers}/{questionsAsked}</h3>
+    <button className="reset" onClick={reset}>Reset</button>
+    <Link to="/">
+      <button>Home</button>
+    </Link>
+  </div>
+)
 }
 
 export default GuessNumber;
