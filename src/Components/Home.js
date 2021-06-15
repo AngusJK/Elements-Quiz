80504481094b94d94096079d54341056cb990209
Angus Kellett <anguskellett@hotmail.com>
2021-06-15 00:06:55 +0000

    adds name input form and game description on links

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -7,16 +7,20 @@ const Home = function() {
     <div className="home">
       <h1>Home Page</h1>
       <h3>Enter your Name</h3>
+      <form action="">
+        <input type="text" className="name-input" placeholder="name"/>
+        <button>Submit</button>
+      </form>
       <h3>Choose a game</h3>
       <ul className="nav-links">
         <Link to="/guessname">
-          <li>Game 1</li>
+          <li>Game 1 - guess the name from the symbol provided</li>
         </Link>
         <Link to="/guessnumber">
-          <li>Game 2</li>
+          <li>Game 2 - guess the atomic number from the name provided</li>
         </Link>
         <Link to="/guesssymbol">
-          <li>Game 3</li>
+          <li>Game 3 - guess the chemical symbol from the name provided</li>
         </Link>
       </ul>
     </div>
