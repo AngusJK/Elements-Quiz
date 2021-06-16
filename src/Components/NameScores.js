cfd534cbbca9b632aeec57c89cce6d1fd8cc0060
Angus Kellett <anguskellett@hotmail.com>
2021-06-16 00:05:50 +0000

    sorts array by score order before rendering

---
+++ src/Components/NameScores.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NameScores = function({ nameScores }) {
+  function compare(a, b) {
+    const scoreA = a.score;
+    const scoreB = b.score;
+    let comparison = 0;
+    if (scoreA > scoreB) {
+      comparison = -1;
+    } else if (scoreA < scoreB) {
+      comparison = 1; 
+    }
+    return comparison;
+  }
+  const scoresInOrder = nameScores.sort(compare);
+  return (
+    <div>
+      <h3>High scores for name game</h3>
+      <ul>
+        {scoresInOrder.map(score => (
+          <li>name: {score.name} score: {score.score}</li>
+        ))}
+      </ul>
+      <Link to="/">
+        Go Back
+      </Link>
+    </div>
+  )
+};
+
+export default NameScores;
