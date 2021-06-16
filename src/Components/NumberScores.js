5ec9dc2eb06dcec68aa6954adfb02013eecd32ef
Angus Kellett <anguskellett@hotmail.com>
2021-06-16 21:41:42 +0000

    adds key to each li

--- src/Components/NumberScores.js
+++ src/Components/NumberScores.js
@@ -2,10 +2,26 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const NumberScores = function({ numberScores }) {
+  function compare(a, b) {
+    const scoreA = a.score;
+    const scoreB = b.score;
+    let comparison = 0;
+    if (scoreA > scoreB) {
+      comparison = -1;
+    } else if (scoreA < scoreB) {
+      comparison = 1; 
+    }
+    return comparison;
+  }
+  const scoresInOrder = numberScores.sort(compare);
   return (
     <div>
-      <h3>High scores for number game</h3>
-      <h5>Name: {numberScores[0].name}, Score: {numberScores[0].score}</h5>
+      <h3>High scores for numbers game</h3>
+      <ul>
+        {scoresInOrder.map(score => (
+          <li key={score.key}>name: {score.name}, score: {score.score}</li>
+        ))}
+      </ul>
       <Link to="/">
         Go Back
       </Link>
