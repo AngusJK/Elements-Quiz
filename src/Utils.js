b78a6fbf679fcf06f78919ce9bd3bc351695581b
Angus Kellett <anguskellett@hotmail.com>
2021-06-16 21:50:49 +0000

    defines compare function

---
+++ src/Utils.js
@@ -0,0 +1,12 @@
+export const compare = function(a, b) {
+  const scoreA = a.score;
+  const scoreB = b.score;
+  let comparison = 0;
+  if (scoreA > scoreB) {
+    comparison = -1;
+  } else if (scoreA < scoreB) {
+    comparison = 1; 
+  }
+  return comparison;
+};
+
