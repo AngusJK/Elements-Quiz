7af465717e23cf584a5893c10c0d99a06241ca93
Angus Kellett <anguskellett@hotmail.com>
2021-06-25 23:17:39 +0000

    puts button elements inside links

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -30,44 +30,49 @@ const Home = function({
   if (userName) {
     return (
       <div className="home-page">
-        <h2>Welcome, {userName}.</h2>
-        <h3>Choose a game to play:</h3>
+        <h5>Welcome, {userName}. Choose a game to play:</h5>
         <ul className="nav-links">
-          <li>Game 1 - guess the name from the symbol provided</li>
-          <Link to="/guessname">
-            <button className="btn play">
-              Play
-            </button>
-          </Link>
-          <Link to="/namescores">
-            <button className="btn high-scores">
-              High Scores
-            </button>
-          </Link>
-          <li>Game 2 - guess the atomic number from the name provided</li>
-          <Link to="/guessnumber">
-            <button className="btn play">
-              Play
-            </button>
-          </Link>
-          <Link to="/numberscores">
-            <button className="btn high-scores">
-              High Scores
-            </button>
-          </Link>
-          <li>Game 3 - guess the chemical symbol from the name provided</li>
-          <Link to="/guesssymbol">
-            <button className="btn play">
-              Play
-            </button>
-          </Link>
-          <Link to="/symbolscores">
-            <button className="btn high-scores">
-              High Scores
-            </button>
-          </Link>
+          <div className="game">
+            <li>Game 1 - guess the name from the symbol provided</li>
+            <Link to="/guessname">
+              <button className="btn play">
+                Play
+              </button>
+            </Link>
+            <Link to="/namescores">
+              <button className="btn high-scores">
+                High Scores
+              </button>
+            </Link>
+          </div>
+          <div className="game">
+            <li>Game 2 - guess the atomic number from the name provided</li>
+            <Link to="/guessnumber">
+              <button className="btn play">
+                Play
+              </button>
+            </Link>
+            <Link to="/numberscores">
+              <button className="btn high-scores">
+                High Scores
+              </button>
+            </Link>
+          </div>
+          <div className="game">
+            <li>Game 3 - guess the chemical symbol from the name provided</li>
+            <Link to="/guesssymbol">
+              <button className="btn play">
+                Play
+              </button>
+            </Link>
+            <Link to="/symbolscores">
+              <button className="btn high-scores">
+                High Scores
+              </button>
+            </Link>
+          </div>
         </ul> 
-        <button onClick={logOutUser} className="logout">Log Out</button>
+        <button onClick={logOutUser} className="btn logout">Log Out</button>
       </div>
     )
   } else {
