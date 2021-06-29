81c349fdabe87a92ae8de41995582bd00b0da63e
Angus Kellett <anguskellett@hotmail.com>
2021-06-29 00:28:34 +0000

    creates table for high scores

--- src/Components/SymbolScores.js
+++ src/Components/SymbolScores.js
@@ -5,15 +5,26 @@ import { compare } from '../Utils';
 const SymbolScores = function({ symbolScores }) {
   const scoresInOrder = symbolScores.sort(compare);
   return (
-    <div>
-      <h3>High scores for symbols game</h3>
-      <ul>
-        {scoresInOrder.map(score => (
-          <li key={score.key}>name: {score.name}, score: {score.score}</li>
-        ))}
-      </ul>
+    <div id="high-scores">
+      <h3>High scores for Game 3</h3>
+      <table className="high-scores">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Score</th>
+          </tr>
+        </thead>
+        <tbody>
+          {scoresInOrder.map(score => (
+            <tr key={score.key}>
+              <td>{score.name}</td>
+              <td>{score.score}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
       <Link to="/">
-        <div><button className="btn">Go Back</button></div>
+        <div><button className="btn escape-btn">Go Back</button></div>
       </Link>
     </div>
   )
