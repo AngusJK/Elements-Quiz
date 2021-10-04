4350bc55cd99aa92b0ca591596ed65c07eee99d5
Angus Kellett <anguskellett@hotmail.com>
2021-10-04 22:43:03 +0000

    removes semicolons; corrects indentation; changes let to const where necessary

--- src/Components/GuessSymbol.js
+++ src/Components/GuessSymbol.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { Link } from 'react-router-dom'
 
-const GuessSymbol = function({ 
-  inputText, 
-  setInputText, 
-  elements, 
-  currentElement, 
+const GuessSymbol = function ({
+  inputText,
+  setInputText,
+  elements,
+  currentElement,
   setCurrentElement,
   questionsAsked, 
   setQuestionsAsked,
@@ -18,67 +18,68 @@ const GuessSymbol = function({
   setSymbolScores,
   gameStarted,
   setGameStarted
-  }) {
-const setNewElementName = () => {
-  let num = Math.floor(Math.random() * 118);
-  setCurrentElement(elements[num]);
-}
-const updateSymbolScores = (name, number) => {
-  const latestScore = { key: symbolScores.length + 1, name: name, score: +number };
-  const newScores = [...symbolScores];
-  newScores.push(latestScore);
-  setSymbolScores(newScores);
-}
-const submitAnswerHandler = (e) => {
-  e.preventDefault();
-  setQuestionsAsked(questionsAsked + 1);
-  if(currentElement.symbol === inputText) {
-    setCorrectAnswers(correctAnswers + 1);
-  } else {
-    setGameOver(true);
-    setGameStarted(false);
-    if(questionsAsked > 0) {updateSymbolScores(userName, questionsAsked)};
+}) {
+  const setNewElementName = () => {
+    const num = Math.floor(Math.random() * 118)
+    setCurrentElement(elements[num])
   }
-  e.target.reset();
-  setNewElementName();
-}
-const inputTextHandler = (e) => {
-  setInputText(e.target.value.trim());
-}
-const reset = () => {
-  setQuestionsAsked(0);
-  setCorrectAnswers(0);
-  setCurrentElement('');
-  setGameOver(false);
-  setGameStarted(false)
-}
-const replay = () => {
-  setQuestionsAsked(0);
-  setCorrectAnswers(0);
-  setNewElementName();
-  setGameOver(false);
-  setGameStarted(true);
-}
-const startGame = () => {
-  setNewElementName();
-  setGameStarted(true);
-}
-const capitalize = function(s) {
-  if (typeof(s) !== 'string') return "";
-  return s.charAt(0).toUpperCase() + s.slice(1);
-}
-if (gameOver) {
-  return(
-    <div className="game-over">
-      <h1 className="game-over-headline">GAME OVER</h1>
-      <h3>Your score: {correctAnswers}</h3>
-      <button className="btn start-game-btn" onClick={replay}>Play Again</button>
-      <Link to="/">
-        <button onClick={reset} className="btn escape-btn">Home</button>
-      </Link>
-    </div>
-  )} else if (gameStarted === false) {
-    return(
+  const updateSymbolScores = (name, number) => {
+    const latestScore = { key: symbolScores.length + 1, name: name, score: +number }
+    const newScores = [...symbolScores]
+    newScores.push(latestScore)
+    setSymbolScores(newScores)
+  }
+  const submitAnswerHandler = (e) => {
+    e.preventDefault()
+    setQuestionsAsked(questionsAsked + 1)
+    if (currentElement.symbol === inputText) {
+      setCorrectAnswers(correctAnswers + 1)
+    } else {
+      setGameOver(true)
+      setGameStarted(false)
+      if (questionsAsked > 0) { updateSymbolScores(userName, questionsAsked) }
+    }
+    e.target.reset()
+    setNewElementName()
+  }
+  const inputTextHandler = (e) => {
+    setInputText(e.target.value.trim())
+  }
+  const reset = () => {
+    setQuestionsAsked(0)
+    setCorrectAnswers(0)
+    setCurrentElement('')
+    setGameOver(false)
+    setGameStarted(false)
+  }
+  const replay = () => {
+    setQuestionsAsked(0)
+    setCorrectAnswers(0)
+    setNewElementName()
+    setGameOver(false)
+    setGameStarted(true)
+  }
+  const startGame = () => {
+    setNewElementName()
+    setGameStarted(true)
+  }
+  const capitalize = function (s) {
+    if (typeof (s) !== 'string') return ''
+    return s.charAt(0).toUpperCase() + s.slice(1)
+  }
+  if (gameOver) {
+    return (
+      <div className="game-over">
+        <h1 className="game-over-headline">GAME OVER</h1>
+        <h3>Your score: {correctAnswers}</h3>
+        <button className="btn start-game-btn" onClick={replay}>Play Again</button>
+        <Link to="/">
+          <button onClick={reset} className="btn escape-btn">Home</button>
+        </Link>
+      </div>
+    )
+  } else if (gameStarted === false) {
+    return (
       <div className="question">
         <p>In this game you will be given the name of an element from the Periodic Table and you must give its atomic number. Questions continue until you get one wrong. Remember to capitalize the first letter! Good luck, {userName}!</p>
         <div><button className="btn start-game-btn" onClick={startGame}>Start Game</button></div>
@@ -97,9 +98,9 @@ if (gameOver) {
         <form action="" className="answer" onSubmit={submitAnswerHandler}>
           <input className="answer-input"
             id="symbol-answer-input"
-            onChange={inputTextHandler} 
-            type="text" 
-            autoComplete="off" 
+            onChange={inputTextHandler}
+            type="text"
+            autoComplete="off"
             maxLength="2"
             required
             autoFocus/>
@@ -113,4 +114,4 @@ if (gameOver) {
   }
 }
 
-export default GuessSymbol;
+export default GuessSymbol
