2355b89ac74de4268a633afe3fb044acacc06ec6
Angus Kellett <anguskellett@hotmail.com>
2021-10-04 22:32:33 +0000

    removes semicolons, fixes spacing

--- src/Components/GuessNumber.js
+++ src/Components/GuessNumber.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { Link } from 'react-router-dom'
 
-const GuessNumber = function({ 
-  inputText, 
-  setInputText, 
-  elements, 
-  currentElement, 
+const GuessNumber = function ({
+  inputText,
+  setInputText,
+  elements,
+  currentElement,
   setCurrentElement,
-  questionsAsked, 
+  questionsAsked,
   setQuestionsAsked,
   correctAnswers,
   setCorrectAnswers,
@@ -18,75 +18,78 @@ const GuessNumber = function({
   setNumberScores,
   gameStarted,
   setGameStarted
-  }) {
-const setNewElementName = () => {
-  let num = Math.floor(Math.random() * 118);
-  setCurrentElement(elements[num]);
-}
-const updateNumberScores = (name, number) => {
-  const latestScore = { key: numberScores.length + 1, name: name, score: +number };
-  const newScores = [...numberScores];
-  newScores.push(latestScore);
-  setNumberScores(newScores);
-}
-const submitAnswerHandler = (e) => {
-  e.preventDefault();
-  setQuestionsAsked(questionsAsked + 1);
-  if(currentElement.atno.toString() === inputText) {
-    setCorrectAnswers(correctAnswers + 1);
-  } else {
-    setGameOver(true);
-    setGameStarted(false);
-    if(questionsAsked > 0) {updateNumberScores(userName, questionsAsked)};
+}) {
+  const setNewElementName = () => {
+    const num = Math.floor(Math.random() * 118)
+    setCurrentElement(elements[num])
   }
-  e.target.reset();
-  setNewElementName();
-}
+  const updateNumberScores = (name, number) => {
+    const latestScore = { key: numberScores.length + 1, name: name, score: +number }
+    const newScores = [...numberScores]
+    newScores.push(latestScore)
+    setNumberScores(newScores)
+  }
+  const submitAnswerHandler = (e) => {
+    e.preventDefault()
+    setQuestionsAsked(questionsAsked + 1)
+    if (currentElement.atno.toString() === inputText) {
+      setCorrectAnswers(correctAnswers + 1)
+    } else {
+      setGameOver(true)
+      setGameStarted(false)
+      if (questionsAsked > 0) { updateNumberScores(userName, questionsAsked) }
+    }
+    e.target.reset()
+    setNewElementName()
+  }
 
-const inputTextHandler = (e) => {
-  setInputText(e.target.value.trim());
-}
-const reset = () => {
-  setQuestionsAsked(0);
-  setCorrectAnswers(0);
-  setCurrentElement('');
-  setGameOver(false);
-  setGameStarted(false);
-}
-const replay = () => {
-  setQuestionsAsked(0);
-  setCorrectAnswers(0);
-  setNewElementName();
-  setGameOver(false);
-  setGameStarted(true);
-}
-const startGame = () => {
-  setNewElementName();
-  setGameStarted(true);
-}
-const capitalize = function(s) {
-  if (typeof(s) !== 'string') return "";
-  return s.charAt(0).toUpperCase() + s.slice(1);
-}
-if (gameOver) { return (
-  <div className="game-over">
-    <h1 className="game-over-headline">GAME OVER</h1>
-    <h3>Your score: {correctAnswers}</h3>
-    <button className="btn start-game-btn" onClick={replay}>Play Again</button>
-    <Link to="/">
-      <button className="btn escape-btn" onClick={reset}>Home</button>
-    </Link>
-  </div>
-)} else if (gameStarted === false) { 
-  return (
-    <div className="question">
-      <p>In this game you will be shown the name of an element and you must type it's Atomic Number. Get as many consecutive answers correct as you can. Good luck, {userName}!</p>
-      <div><button className="btn start-game-btn" onClick={startGame}>Start Game</button></div>
+  const inputTextHandler = (e) => {
+    setInputText(e.target.value.trim())
+  }
+  const reset = () => {
+    setQuestionsAsked(0)
+    setCorrectAnswers(0)
+    setCurrentElement('')
+    setGameOver(false)
+    setGameStarted(false)
+  }
+  const replay = () => {
+    setQuestionsAsked(0)
+    setCorrectAnswers(0)
+    setNewElementName()
+    setGameOver(false)
+    setGameStarted(true)
+  }
+  const startGame = () => {
+    setNewElementName()
+    setGameStarted(true)
+  }
+  const capitalize = function (s) {
+    if (typeof (s) !== 'string') return ''
+    return s.charAt(0).toUpperCase() + s.slice(1)
+  }
+  if (gameOver) {
+    return (
+    <div className="game-over">
+      <h1 className="game-over-headline">GAME OVER</h1>
+      <h3>Your score: {correctAnswers}</h3>
+      <button className="btn start-game-btn" onClick={replay}>Play Again</button>
       <Link to="/">
-        <button className="btn escape-btn">Yikes! Get me out of here!</button>
+        <button className="btn escape-btn" onClick={reset}>Home</button>
       </Link>
     </div>
-  )} else {
+    )
+  } else if (gameStarted === false) {
+    return (
+      <div className="question">
+        <p>In this game you will be shown the name of an element and you must type it's Atomic Number. Get as many consecutive answers correct as you can. Good luck, {userName}!</p>
+        <div><button className="btn start-game-btn" onClick={startGame}>Start Game</button></div>
+        <Link to="/">
+          <button className="btn escape-btn">Yikes! Get me out of here!</button>
+        </Link>
+      </div>
+    )
+  } else {
     return (
       <div className="game-on">
         <p>What is the Atomic Number of this element?:</p>
@@ -112,4 +115,4 @@ if (gameOver) { return (
   }
 }
 
-export default GuessNumber;
+export default GuessNumber
