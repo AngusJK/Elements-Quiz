0636f92067f74eaa69231752d09cf14dd4ae032d
Angus Kellett <anguskellett@hotmail.com>
2021-10-05 18:53:59 +0000

    imports PropTypes and validates props

--- src/Components/NameScores.js
+++ src/Components/NameScores.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import { compare } from '../Utils'
 
@@ -35,6 +36,10 @@ const NameScores = function ({ nameScores }) {
       </Link>
     </div>
   )
+}
+
+NameScores.propTypes = {
+  nameScores: PropTypes.array
 }
 
 export default NameScores
