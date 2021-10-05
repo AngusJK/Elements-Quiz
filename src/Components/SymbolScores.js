c5bba20857cf1437f8f6c2bfbdd034dd63cc3919
Angus Kellett <anguskellett@hotmail.com>
2021-10-05 18:54:10 +0000

    imports PropTypes and validates props

--- src/Components/SymbolScores.js
+++ src/Components/SymbolScores.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import { compare } from '../Utils'
 
@@ -35,6 +36,10 @@ const SymbolScores = function ({ symbolScores }) {
       </Link>
     </div>
   )
+}
+
+SymbolScores.propTypes = {
+  symbolScores: PropTypes.array
 }
 
 export default SymbolScores
